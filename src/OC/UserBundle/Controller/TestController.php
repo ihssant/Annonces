b266d34be378a6aeeb9f6ed9f694ae2f2a1ec809
itaziny <itaziny@ITAZINY-PC.envivio.com>
2015-03-26 15:21:47 +0100

    Ajout du controlleur TestController

---
+++ src/OC/UserBundle/Controller/TestController.php
@@ -0,0 +1,12 @@
+<?php
+namespace OC\UserBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+
+class AdvertController extends Controller
+{
+	/** Ceci est un controlleur de la branch test */
+}
